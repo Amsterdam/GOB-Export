ad8dbdf38f5010ba3fec2c73afa6d034f6f9d8ec
Jasper Jager <mail@jasperjager.com>
2018-11-08 14:30:26 +0100

    Update export and types to match specs
    The option was added to add precision to numbers, and empty string now do not wrap in $$. Export configs were updated to match requirements. Gobcore updated to latest version

--- src/gobexport/meetbouten/config.py
+++ src/gobexport/meetbouten/config.py
@@ -5,13 +5,14 @@ class MeetboutExportConfig:
         meetboutid: 22081299:
         http://localhost:5000/meetbouten/meetbouten/?id=22081299
         =>
-        $$22081299$$|$$$$|120951|491840|-1,9926||$$20010920$$|1||$$$$|$$N$$|$$Orionplantsoen 7$$|$$$$||$$A$$
+        $$22081299$$|$$$$|120951|491840|-1,9926||$$20010920$$|||$$$$|$$N$$|$$Orionplantsoen 7$$|$$$$||$$A$$
         |$$PB15 $$|1|POINT (120951.0 491840.0)
 
     """
-    format = 'meetboutid:str|buurt:str|geometrie:xco|geometrie:yco|hoogte_tov_nap:num|zakking_cumulatief:num|' \
-             'datum:dat|bouwblokzijde:num|eigenaar:num|indicatie_beveiligd:bool|stadsdeel:str|nabij_adres_text:str|' \
-             'locatie:str|zakkingssnelheid:num|status:str|bouwblok:str|blokeenheid:num|geometrie:geo'
+    format = 'identificatie:str|buurt:str|geometrie:coo:x|geometrie:coo:y|hoogte_tov_nap:num:4|' \
+             'zakking_cumulatief:num:1|datum:dat|bouwblokzijde:num|eigenaar:num|indicatie_beveiligd:str|' \
+             'stadsdeel:str|adres:str|locatie:str|zakkingssnelheid:num:1|status:str|bouwblok:str|' \
+             'blokeenheid:num|geometrie:geo'
     path = '/gob/meetbouten/meetbouten/?view=enhanced'
 
 
@@ -27,8 +28,8 @@ class MetingenExportConfig:
         |$$Fugro$$|3|274||$$K$$|$$W$$
 
     """
-    format = 'metingid:num|datum:dat|type_meting:str|hoogte_tov_nap:num|zakking:num|hoort_bij_meetbout_id:str|' \
-             'refp1_nr:str|refp2_nr:str|refp3_nr:str|zakkingssnelheid:num|zakking_cumulatief:num|' \
+    format = 'identificatie:num|datum:dat|type_meting:str|hoogte_tov_nap:num:4|zakking:num:1|hoort_bij_meetbout:str|' \
+             'refp1_nr:str|refp2_nr:str|refp3_nr:str|zakkingssnelheid:num:1|zakking_cumulatief:num:1|' \
              'is_gemeten_door:str|hoeveelste_meting:num|aantal_dagen:num|pandmsl:str|' \
              'ligt_in_stadseel_text:str|wvi:str'
     path = '/gob/meetbouten/metingen/?view=enhanced'
@@ -44,9 +45,9 @@ class ReferentiepuntenExportConfig:
         |POINT (121155.0 489090.0)
 
     """
-    format = 'referentiepuntid:num|geometrie:xco|geometrie:yco|' \
-             'hoogte_tov_nap:num|datum:dat|locatie:str|geometrie:geo'
-    path = '/gob/meetbouten/referentiepunten/'
+    format = 'identificatie:num|geometrie:coo:x|geometrie:coo:y|' \
+             'hoogte_tov_nap:num:4|datum:dat|locatie:str|geometrie:geo'
+    path = '/gob/meetbouten/referentiepunten/?view=enhanced'
 
 
 class RollagenExportConfig:
@@ -58,5 +59,5 @@ class RollagenExportConfig:
         $$AK25$$|1|121287|485235|POINT (121287.0 485245.0)
 
     """
-    format = 'rollaagid:str|idx:num|geometrie:xco|geometrie:yco|geometrie:geo'
+    format = 'identificatie:str|idx:num|geometrie:coo:x|geometrie:coo:y|geometrie:geo'
     path = '/gob/meetbouten/rollagen/?view=enhanced'
