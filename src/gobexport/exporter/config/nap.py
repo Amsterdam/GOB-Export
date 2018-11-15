1e27f5dd203c36e423bea2057424f10a1f6bbba0
Jasper Jager <mail@jasperjager.com>
2018-11-15 09:04:26 +0100

    Update to example

--- src/gobexport/exporter/config/nap.py
+++ src/gobexport/exporter/config/nap.py
@@ -2,11 +2,11 @@ class PeilmerkenExportConfig:
     """Peilmerken config
 
     Example:
-        identificatie: 22081299:
-        http://localhost:5000/gob/nap/peilmerken/?id=22081299
+        identificatie: 11080002:
+        http://localhost:5000/gob/nap/peilmerken/?id=11080002
         =>
-        $$22081299$$|$$$$|120951|491840|-1,9926||$$20010920$$|||$$$$|$$N$$|$$Orionplantsoen 7$$|$$$$||$$A$$
-        |$$PB15 $$|1|POINT (120951.0 491840.0)
+        $$11080002$$|1,538|2018|3|$$Chassestraat 59 "Wassenaerschool"(x = inspringende hoek; y = bovenkant
+        gemetselde rollaag van de plint)$$|$$O$$|-31,0|83,0||POINT (119193.3 486914.2)
 
     """
     format = 'identificatie:str|hoogte_tov_nap:num:3|jaar:num|merk_id:num|omschrijving:str|' \
