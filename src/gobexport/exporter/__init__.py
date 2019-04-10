0ec89bad92397aa916b8ec3100f835dd696a15c1
Jasper Jager <mail@jasperjager.com>
2019-04-10 13:40:54 +0200

    Add graphql API
    Use the new graphql API iterator to generate the CSV with history for gebieden. The graphql output is flattened and a history converter translates the references into timeslots.

--- src/gobexport/exporter/__init__.py
+++ src/gobexport/exporter/__init__.py
@@ -6,7 +6,7 @@ from gobexport.exporter.config.meetbouten import MeetboutExportConfig, MetingenE
                                      ReferentiepuntenExportConfig, RollagenExportConfig
 from gobexport.exporter.config.nap import PeilmerkenExportConfig
 from gobexport.exporter.config.test import TestEntityExportConfig
-
+from gobexport.graphql import GraphQL
 
 CONFIG_MAPPING = {
     'test_catalogue': {
@@ -32,19 +32,31 @@ CONFIG_MAPPING = {
 }
 
 
-def export_to_file(host, endpoint, exporter, file, format):
+def export_to_file(host, product, file, catalogue, collection):
     """Export a collection from a catalog a file.
 
     The entities that are exposed by the specified API host are retrieved, converted and written to
     the specified output file
 
     :param host: The API host
-    :param endpoint: The API endpoint
-    :param exporter: The function used to write to file
+    :param product: The product definition for this export type
     :param file: The name of the file to write the ouput
+    :param catalogue: The catalogue to export
+    :param collection: The collection to export
     :return: The number of exported rows
     """
-    api = API(host=host, path=endpoint)
+    if product.get('api_type') == 'graphql':
+        # Use GraphQL
+        query = product['query']
+        expand_history = product.get('expand_history')
+        api = GraphQL(host, query, catalogue, collection, expand_history)
+    else:
+        # Use the REST API
+        endpoint = product.get('endpoint')
+        api = API(host=host, path=endpoint)
+
+    exporter = product.get('exporter')
+    format = product.get('format')
 
     row_count = exporter(api, file, format)
 
