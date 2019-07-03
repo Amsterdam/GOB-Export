ec3dd7a01b07f5e31594a2f772c6a75a4a035c1a
vegter <amsjurien@gmail.com>
2019-07-03 09:25:08 +0200

    Improve exception handling of requests module
    Catch all request exceptions
    Improve logging

--- src/gobexport/requests.py
+++ src/gobexport/requests.py
@@ -7,7 +7,7 @@ _RETRY_TIMEOUT = 60     # Seconds between consecetive retries
 _REQUEST_TIMEOUT = 300  # Request timout to 300 seconds to get a response
 
 
-class RequestException(IOError):
+class APIException(IOError):
     pass
 
 
@@ -15,7 +15,7 @@ def _exec(method, **kwargs):
     """
     Execute method _MAX_TRIES times to get a result
 
-    :raise RequestException when all _MAX_TRIES requests have failed
+    :raise APIException when all _MAX_TRIES requests have failed
     :param method: get or post
     :param kwargs: any arguments
     :return: the result of the get or post request
@@ -23,15 +23,25 @@ def _exec(method, **kwargs):
     n_tries = 0
     while n_tries < _MAX_TRIES:
         try:
-            return method(**kwargs)
-        except requests.RequestException as e:
+            response = method(**kwargs)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            # Errors and Exceptions
+            #
+            # Network problem (e.g. DNS failure, refused connection, etc) => ConnectionError
+            # HTTPError if the HTTP request returned an unsuccessful status code.
+            # If a request times out, a Timeout exception is raised.
+            # If a request exceeds the configured number of maximum redirections => TooManyRedirects
+            #
+            # All exceptions that Requests explicitly raises inherit from requests.exceptions.RequestException.
             n_tries += 1
             print(f"Request exception: '{str(e)}'")
             print(f"Retry in {_RETRY_TIMEOUT} seconds, retries left: {_MAX_TRIES - n_tries}")
             time.sleep(_RETRY_TIMEOUT)
 
     request = ', '.join([f"{k}='{v}'" for k, v in kwargs.items()])
-    raise RequestException(f"Request '{request}' failed, tried {n_tries} times")
+    raise APIException(f"Request '{request}' failed, tried {n_tries} times")
 
 
 def get(url):
