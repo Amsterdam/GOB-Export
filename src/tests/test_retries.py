ba71ce989f697331617981544b6b3dceab1361b1
vegter <amsjurien@gmail.com>
2019-09-24 12:06:11 +0200

    Export with automatic retries

---
+++ src/tests/test_retries.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+from gobexport.export import _with_retries
+
+class SucceedAfterNTimes():
+
+    def __init__(self, n_tries):
+        self.n_tries = n_tries
+        self.n_exec = 0
+
+    def f(self, value, exc):
+        self.n_exec += 1
+        self.n_tries -= 1
+        if self.n_tries > 0:
+            raise exc
+        return value
+
+class TestRetries(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_no_exec(self):
+        # No execution
+        result = _with_retries(lambda: 0, 0, Exception)
+        self.assertIsNone(result)
+
+    def test_no_exception(self):
+        result = _with_retries(lambda: 0, 1, Exception)
+        self.assertEqual(result, 0)
+
+        result = _with_retries(lambda: 0, 2, Exception)
+        self.assertEqual(result, 0)
+
+    def test_exception(self):
+        # retries is sufficient
+        test_retry = SucceedAfterNTimes(2)
+        method = lambda: test_retry.f("result", KeyError)
+        result = _with_retries(method, 2, KeyError)
+        self.assertEqual(result, "result")
+        self.assertEqual(test_retry.n_exec, 2)
+
+        # retries is not sufficient
+        test_retry = SucceedAfterNTimes(2)
+        method = lambda: test_retry.f("result", KeyError)
+        with self.assertRaises(KeyError):
+            result = _with_retries(method, 1, KeyError)
+        self.assertEqual(test_retry.n_exec, 1)
+
+        # Only catch the given exception
+        test_retry = SucceedAfterNTimes(2)
+        method = lambda: test_retry.f("result", AttributeError)
+        with self.assertRaises(AttributeError):
+            result = _with_retries(method, 1, KeyError)
+        self.assertEqual(test_retry.n_exec, 1)
