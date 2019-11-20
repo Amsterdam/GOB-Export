77a6ce88572803f608b87f2291179a1c12b47909
Jaap Koetsier <jaap@chariot-it.nl>
2019-11-20 14:39:46 +0100

    Rewrite undouble method nicer, add tests

--- src/tests/formatter/test_graphql.py
+++ src/tests/formatter/test_graphql.py
@@ -1,3 +1,5 @@
+import random
+
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
@@ -61,6 +63,15 @@ class TestGraphQLResultFormatter(TestCase):
         formatter._flatten_edge = lambda x: 'flattened(' + x + ')'
 
         self.assertEqual(['flattened(formatted_row(a))'], list(formatter.format_item('a')))
+
+    def test_undouble(self):
+        # Generate random lists of integers, plus add empty list
+        testcases = [[random.randint(0, 10) for _ in range(random.randint(0, 50))] for _ in range(20)] + [[]]
+
+        formatter = GraphQLResultFormatter()
+
+        for testcase in testcases:
+            self.assertEqual(sorted(list(set(testcase))), sorted(formatter._undouble(testcase)))
 
     def test_flatten_edge(self):
         formatter = GraphQLResultFormatter()
