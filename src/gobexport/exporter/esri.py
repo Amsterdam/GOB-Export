7d7473b7bdbc62d7df43b4c9534395e8a50bcc68
Nick Barykine <nick.barykine@gmail.com>
2019-12-02 14:09:03 +0100

    [GOB-1130] Add docstrings and some tests to coordinates calculation

--- src/gobexport/exporter/esri.py
+++ src/gobexport/exporter/esri.py
@@ -19,6 +19,7 @@ spatialref_wgs84 = osr.SpatialReference()
 spatialref_wgs84.ImportFromEPSG(4326)
 
 transform = osr.CoordinateTransformation(spatialref_rd, spatialref_wgs84)
+COORDINATE_PRECISION = 7
 
 
 def add_field_definitions(layer, fieldnames):
@@ -126,24 +127,44 @@ def esri_exporter(api, file, format=None, append=False, filter: EntityFilter=Non
 
 
 def get_centroid(wkt):
+    """Get Centroid from WKT geomentry.
+
+    We need POINT to be able to get coordinates.
+    """
     return ogr.CreateGeometryFromWkt(wkt).Centroid()
 
 
 def get_x(wkt):
+    """Get X coordinate from Centroid of WKT geomentry.
+
+    Also cast it to integer.
+    """
     return int(get_centroid(wkt).GetX())
 
 
 def get_y(wkt):
+    """Get Y coordinate from Centroid of WKT geomentry.
+
+    Also cast it to integer.
+    """
     return int(get_centroid(wkt).GetY())
 
 
 def get_longitude(wkt):
+    """Get Longitude coordinate (WGS84) from Centroid of WKT geomentry.
+
+    Also round it to COORDINATE_PRECISION (decimal percision).
+    """
     centroid = get_centroid(wkt)
     centroid.Transform(transform)
-    return round(centroid.GetX(), 7)
+    return round(centroid.GetX(), COORDINATE_PRECISION)
 
 
 def get_latitude(wkt):
+    """Get Latitude coordinate (WGS84) from Centroid of WKT geomentry.
+
+    Also round it to COORDINATE_PRECISION (decimal percision).
+    """
     centroid = get_centroid(wkt)
     centroid.Transform(transform)
-    return round(centroid.GetY(), 7)
+    return round(centroid.GetY(), COORDINATE_PRECISION)
