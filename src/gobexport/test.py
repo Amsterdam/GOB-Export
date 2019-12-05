7c34b30dee23f8d0ab0c384ec7b06bbda554bc4c
Jaap Koetsier <jaap@chariot-it.nl>
2019-12-05 11:08:49 +0100

    GOB-1289 Remove silently ignoring exception in export test file download

--- src/gobexport/test.py
+++ src/gobexport/test.py
@@ -129,14 +129,11 @@ def _get_file(conn_info, filename):
     :param filename: name of the file to retrieve
     :return:
     """
-    try:
-        for item in get_full_container_list(conn_info['connection'], conn_info['container']):
-            if item["name"] == filename:
-                obj_info = dict(item)
-                obj = get_object(conn_info['connection'], item, conn_info['container'])
-                return obj_info, obj
-    except Exception:
-        pass
+    for item in get_full_container_list(conn_info['connection'], conn_info['container']):
+        if item["name"] == filename:
+            obj_info = dict(item)
+            obj = get_object(conn_info['connection'], item, conn_info['container'])
+            return obj_info, obj
 
     return None, None
 
