e770a2f6255c47b394e91683242d2e8e38c9a9c3
vegter <amsjurien@gmail.com>
2020-02-04 12:42:08 +0100

    Use response worker for streaming requests

---
+++ src/gobexport/worker.py
@@ -0,0 +1,66 @@
+import requests
+
+from gobexport.config import get_host
+
+
+class Worker():
+
+    # Header values for Worker Requests and Responses
+    _WORKER_REQUEST = "X-Worker-Request"
+    _WORKER_ID_RESPONSE = "X-Worker-Id"
+    _WORKER_RESULT_OK = "OK"
+    _WORKER_RESULT_FAILURE = "FAILURE"
+
+    headers = {
+        _WORKER_REQUEST: 'true'
+    }
+
+    @classmethod
+    def handle_response(cls, result):
+        """
+        Handle a worker response.
+
+        Read all lines
+        Check if last line is OK
+        Then get the response file
+        And stream its contents
+        Finally delete the response file from the server
+
+        :param result:
+        :return:
+        """
+        worker_id = result.headers[cls._WORKER_ID_RESPONSE]
+
+        print(f"Worker response {worker_id} started")
+        lastline = None
+        for line in result.iter_lines():
+            lastline = line
+
+        lastline = lastline.decode()
+        if lastline == cls._WORKER_RESULT_FAILURE:
+            print(f"Worker response {worker_id} failed")
+            raise requests.exceptions.RequestException("Worker response failed")
+        elif lastline != cls._WORKER_RESULT_OK:
+            print(f"Worker response {worker_id} ended prematurely")
+            raise requests.exceptions.RequestException("Worker response ended prematurely")
+        else:
+            print(f"Worker result {worker_id} OK")
+            try:
+                # Request result
+                url = f"{get_host()}/gob/worker/{worker_id}"
+                result = requests.get(url=url, stream=True)
+                result.raise_for_status()
+
+                # yield result
+                for line in result.iter_lines():
+                    yield line
+            except Exception as e:
+                print(f"Worker result {worker_id} failed")
+                # Re-raise exception
+                raise e
+            finally:
+                # Always try to cleanup worker files (even if an exception has occurred)
+                print(f"Worker result {worker_id} clear...")
+                url = f"{get_host()}/gob/worker/end/{worker_id}"
+                result = requests.delete(url=url)
+                result.raise_for_status()
