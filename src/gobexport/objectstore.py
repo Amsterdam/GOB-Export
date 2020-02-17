e45b663a8957ab99fb5cd5cea50e9466ab2158d3
Jasper Jager <mail@jasperjager.com>
2020-02-17 17:29:55 +0100

    Add objectstore API and code table exports

---
+++ src/gobexport/objectstore.py
@@ -0,0 +1,52 @@
+"""Objectstore
+
+Encapsulates a file read from Objectstore into an iterator
+
+"""
+from gobcore.database.connector import connect_to_objectstore
+from gobcore.database.reader import query_objectstore
+
+from gobexport.config import get_objectstore_config
+
+
+class Objectstore:
+
+    def __init__(self, config, row_formatter=None):
+        """Constructor
+
+        Lazy loading, Just register objectstore connection and reader and wait for the iterator to be called
+        to load the data
+
+        :param config:
+        """
+        self.config = config
+        self.objectstore_config = get_objectstore_config(self.config['objectstore'])
+        self.connection, self.user = connect_to_objectstore(self.objectstore_config)
+        self.row_formatter = row_formatter
+
+    def __repr__(self):
+        """Representation
+
+        Provide for a readable representation
+        """
+        return f"Objectstore {self.objectstore_config['TENANT_NAME']}"
+
+    def __iter__(self):
+        """Iteration method
+
+        Reads the file and returns enitities
+
+        Raises:
+            AssertionError: if file cannot be read
+
+        :return:
+        """
+        query = query_objectstore(self.connection, self.config)
+        for result in query:
+            yield self.format_item(result)
+
+    def format_item(self, item):
+        if self.row_formatter:
+            return self.row_formatter(item)
+        else:
+            return item
---
+++ src/tests/test_objectstore.py
@@ -0,0 +1,54 @@
+import random
+
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from gobexport.objectstore import Objectstore
+
+
+class TestObjectstore(TestCase):
+    
+    @patch("gobexport.objectstore.get_objectstore_config")
+    @patch("gobexport.objectstore.connect_to_objectstore")
+    @patch("gobexport.objectstore.query_objectstore")
+    def test_objectstore(self, mock_query_objectstore, mock_connect, mock_get_objectstore_config):
+        mock_config = {
+            'objectstore': 'the objectstore',
+        }
+        
+        mock_get_objectstore_config.return_value = {
+            'TENANT_NAME': 'mock_objectstore'
+        }
+        
+        mock_objectstore_config = mock_get_objectstore_config.return_value
+        mock_connect.return_value = ('mock_connection', 'mock_user')
+
+        objectstore = Objectstore(mock_config, row_formatter=None)
+        mock_connect.assert_called_with(mock_objectstore_config)
+        
+        mock_query_objectstore.return_value = [1,2,3]
+        
+        result = [i for i in objectstore]
+        self.assertEqual(result, [1, 2, 3])
+        
+        mock_query_objectstore.assert_called_with(objectstore.connection, objectstore.config)
+        
+        assert(str(objectstore) == 'Objectstore mock_objectstore')
+
+    @patch("gobexport.objectstore.get_objectstore_config")
+    @patch("gobexport.objectstore.connect_to_objectstore")
+    @patch("gobexport.objectstore.query_objectstore")
+    def test_objectstore_with_formatter(self, mock_query_objectstore, mock_connect, mock_get_objectstore_config):
+        mock_config = {
+            'objectstore': 'the objectstore',
+        }
+
+        row_formatter = lambda x: x + 1
+
+        mock_connect.return_value = ('mock_connection', 'mock_user')
+
+        objectstore = Objectstore(mock_config, row_formatter=row_formatter)
+        mock_query_objectstore.return_value = [1,2,3]
+
+        result = [i for i in objectstore]
+        self.assertEqual(result, [2, 3, 4])
