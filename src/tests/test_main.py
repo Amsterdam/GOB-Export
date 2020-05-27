f782ba886ac5645d5c09faeb4c9c8acb02ed046e
Jaap Koetsier <jaap@chariot-it.nl>
2020-05-27 15:19:35 +0200

    PGB-1365 Trigger DumpNotification after completed dump

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,85 +1,115 @@
-from unittest import mock
+from unittest import mock, TestCase
 
-@mock.patch('gobcore.logging.logger.logger', mock.MagicMock())
-@mock.patch('gobcore.message_broker.notifications.listen_to_notifications', mock.MagicMock())
-@mock.patch('gobcore.message_broker.notifications.get_notification')
-@mock.patch('gobcore.workflow.start_workflow.start_workflow')
-@mock.patch('gobexport.test.test')
-@mock.patch('gobexport.export.export')
-@mock.patch('gobexport.dump.Dumper')
-@mock.patch('gobcore.message_broker.messagedriven_service.messagedriven_service')
-def test_main(mocked_messagedriven_service, mocked_dump, mocked_export, mocked_test, mock_start_workflow, mock_get_notification):
+from gobexport import __main__
 
-    from gobexport import __main__
 
-    msg = {
-        "header": {
-            "catalogue": "catalogue",
-            "collection": "collection",
-            "product": "csv",
-            "destination": "Objectstore",
-        },
-        "any other arg": "any other arg",
-    }
+@mock.patch('gobexport.__main__.logger', mock.MagicMock())
+class TestMain(TestCase):
 
-    __main__.handle_export_msg(msg)
+    @mock.patch("gobexport.__main__.messagedriven_service")
+    def test_messagedriven_service(self, mocked_messagedriven_service):
+        from gobexport import __main__ as module
 
-    mocked_messagedriven_service.assert_called_with(__main__.SERVICEDEFINITION, "Export")
-    mocked_export.assert_called_with(
-        catalogue="catalogue",
-        collection="collection",
-        product="csv",
-        destination="Objectstore")
+        with mock.patch.object(module, '__name__', '__main__'):
+            __main__.init()
+            mocked_messagedriven_service.assert_called_with(__main__.SERVICEDEFINITION, "Export")
 
-    msg['header']['destination'] = "Database"
-    __main__.handle_export_msg(msg)
-    mocked_dump.return_value.dump_catalog_assert_called_with(
-        catalogue="catalogue",
-        collection="collection")
 
-    mocked_export.reset_mock()
-    mocked_dump.return_value.dump_catalog.reset_mock()
+    @mock.patch('gobexport.__main__.get_notification')
+    @mock.patch('gobexport.__main__.start_workflow')
+    @mock.patch('gobexport.__main__.test')
+    @mock.patch('gobexport.__main__.export')
+    @mock.patch('gobexport.__main__.Dumper')
+    def test_main(self, mocked_dump, mocked_export, mocked_test, mock_start_workflow, mock_get_notification):
 
-    msg['header']['destination'] = "Unkown destination"
-    __main__.handle_export_msg(msg)
-    mocked_export.assert_not_called()
-    mocked_dump.return_value.dump_catalog_assert_not_called()
-
-    msg = {
-        "header": {
-            "catalogue": "catalogue",
+        msg = {
+            "header": {
+                "catalogue": "catalogue",
+                "collection": "collection",
+                "product": "csv",
+                "destination": "Objectstore",
+            },
+            "any other arg": "any other arg",
         }
-    }
 
-    __main__.handle_export_test_msg(msg)
+        __main__.handle_export_msg(msg)
 
-    mocked_test.assert_called_with("catalogue")
+        mocked_export.assert_called_with(
+            catalogue="catalogue",
+            collection="collection",
+            product="csv",
+            destination="Objectstore")
 
-    header = {
-        'catalogue': 'any catalogue',
-        'collection': 'any collection'
-    }
+        msg['header']['destination'] = "Database"
+        __main__.handle_export_msg(msg)
+        mocked_dump.return_value.dump_catalog_assert_called_with(
+            catalogue="catalogue",
+            collection="collection")
 
-    mock_notification = mock.MagicMock()
-    mock_notification.contents = {
-        'last_event': [1, 1]
-    }
-    mock_get_notification.return_value = mock_notification
-    mock_notification.header = header
+        mocked_export.reset_mock()
+        mocked_dump.return_value.dump_catalog.reset_mock()
 
-    msg = 'any msg'
-    __main__.dump_on_new_events(msg)
-    expected_arguments = {
-        'catalogue': 'any catalogue',
-        'collection': 'any collection',
-        'destination': 'Database',
-        'include_relations': False,
-        'wait_if_job_already_runs': True
-    }
-    mock_start_workflow.assert_not_called()
-    mock_notification.contents = {
-        'last_event': [1, 2]
-    }
-    __main__.dump_on_new_events(msg)
-    mock_start_workflow.assert_called_with({'workflow_name': 'export'}, expected_arguments)
+        msg['header']['destination'] = "Unkown destination"
+        __main__.handle_export_msg(msg)
+        mocked_export.assert_not_called()
+        mocked_dump.return_value.dump_catalog_assert_not_called()
 
+        msg = {
+            "header": {
+                "catalogue": "catalogue",
+            }
+        }
+
+        __main__.handle_export_test_msg(msg)
+
+        mocked_test.assert_called_with("catalogue")
+
+        header = {
+            'catalogue': 'any catalogue',
+            'collection': 'any collection'
+        }
+
+        mock_notification = mock.MagicMock()
+        mock_notification.contents = {
+            'last_event': [1, 1]
+        }
+        mock_get_notification.return_value = mock_notification
+        mock_notification.header = header
+
+        msg = 'any msg'
+        __main__.dump_on_new_events(msg)
+        expected_arguments = {
+            'catalogue': 'any catalogue',
+            'collection': 'any collection',
+            'destination': 'Database',
+            'include_relations': False,
+            'wait_if_job_already_runs': True
+        }
+        mock_start_workflow.assert_not_called()
+        mock_notification.contents = {
+            'last_event': [1, 2]
+        }
+        __main__.dump_on_new_events(msg)
+        mock_start_workflow.assert_called_with({'workflow_name': 'export'}, expected_arguments)
+
+    @mock.patch("gobexport.__main__.Dumper")
+    @mock.patch("gobexport.__main__.DumpNotification")
+    @mock.patch("gobexport.__main__.add_notification")
+    def test_handle_export_dump_msg(self, mock_add_notification, mock_dump_notification, mock_dumper):
+
+        msg = {
+            'header': {
+                'catalogue': 'CAT',
+                'collection': 'COLL',
+                'include_relations': 'TrueOrFalse'
+            }
+        }
+
+        __main__.handle_export_dump_msg(msg)
+
+        mock_dumper().dump_catalog.assert_called_with(catalog_name='CAT',
+                                                      collection_name='COLL',
+                                                      include_relations='TrueOrFalse')
+
+        mock_add_notification.assert_called_with(msg, mock_dump_notification.return_value)
+        mock_dump_notification.assert_called_with('CAT', 'COLL')
