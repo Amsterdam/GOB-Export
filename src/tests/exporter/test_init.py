71ec474285376b2e12c4895a8b77b83bff0debc8
Jasper Jager <mail@jasperjager.com>
2020-11-18 09:09:48 +0100

    Add encryption option to export
    Uses symmetric encryption for the file and asymmetric encryption of the symmetric key. The key is added to the file so the private key of DIA can be used to decrypt the key and then use the key to decrypt the original file.

--- src/tests/exporter/test_init.py
+++ src/tests/exporter/test_init.py
@@ -26,3 +26,19 @@ class TestInit(TestCase):
         product['append'] = False
         self.assertEqual(exporter(), export_to_file('host', product, 'file', 'catalogue', 'collection'))
         exporter.assert_called_with(ANY, 'file', 'the format', append=False, filter=None)
+
+    @patch("gobexport.exporter._init_api", MagicMock())
+    @patch("gobexport.exporter.BufferedIterable", MagicMock())
+    @patch("gobexport.exporter.encrypt_file")
+    def test_export_encrypt_file(self, mock_encrypt_file):
+
+        exporter = MagicMock()
+        product = {
+            'exporter': exporter,
+            'format': 'the format',
+            'encryption_key': 'any key'
+        }
+
+        # Do append, pass value of append_to_filename to exporter
+        export_to_file('host', product, 'file', 'catalogue', 'collection')
+        mock_encrypt_file.assert_called_with('file', 'any key')
--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -1,6 +1,10 @@
 import importlib
 import os
 
+import unittest
+from unittest import mock
+
+from gobexport.config import _getenv, get_public_key
 
 class MockArgumentParser:
 
@@ -29,3 +33,48 @@ def test_host(monkeypatch):
     from gobexport.config import get_host
 
     assert(get_host() == os.getenv('API_HOST', 'http://localhost:8141'))
+
+
+class TestConfig(unittest.TestCase):
+
+    @mock.patch("os.getenv")
+    def test_getenv_notset(self, mock_getenv):
+        mock_getenv.side_effect = lambda varname, value=None: value
+
+        # Test for variable that is not set
+        with self.assertRaises(AssertionError):
+            _getenv("SOME UNKNOWN VARIABLE")
+
+        # Test for optional value
+        value = _getenv("UNSET VARIABLE", is_optional=True)
+        self.assertIsNone(value)
+
+        # Test for variable is not set but default value is given
+        value = _getenv("SOME UNKNOWN VARIABLE", "DEFAULT VALUE")
+        self.assertEqual(value, "DEFAULT VALUE")
+
+    @mock.patch("os.getenv")
+    def test_getenv_set(self, mock_getenv):
+        mock_getenv.side_effect = lambda varname, value=None: "value"
+
+        # Test for variable that is set
+        value = _getenv("SOME KNOWN VARIABLE")
+        self.assertEqual(value, "value")
+
+        # But empty values are not allowed
+
+        mock_getenv.side_effect = lambda varname, value=None: ""
+
+        # Not as default value
+        with self.assertRaises(AssertionError):
+            _getenv("SOME KNOWN VARIABLE", "")
+
+        # Not as variable value
+        with self.assertRaises(AssertionError):
+            _getenv("SOME KNOWN VARIABLE")
+
+    @mock.patch("gobexport.config._getenv")
+    def test_get_public_key(self, mock_getenv):
+        get_public_key("value")
+
+        mock_getenv.assert_called_with('PUBLIC_KEY_VALUE')
