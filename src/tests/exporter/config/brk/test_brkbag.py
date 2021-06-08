febc50e3b9467316581a2e5d8853054e51c6d4c6
Jasper Jager <mail@jasperjager.com>
2021-06-08 09:12:16 +0200

    Remove check on status codes for BRKBAG export

--- src/tests/exporter/config/brk/test_brkbag.py
+++ src/tests/exporter/config/brk/test_brkbag.py
@@ -15,7 +15,6 @@ class TestBrkBagExportConfig(TestCase):
 
         entity = {
             'heeftEenRelatieMetVerblijfsobject.[0].identificatie': None,
-            'heeftEenRelatieMetVerblijfsobject.[0].status.code': 20,
             'heeftEenRelatieMetVerblijfsobject.[0].broninfo.woonplaatsnaam': 'not Amsterdam'
         }
 
@@ -24,7 +23,6 @@ class TestBrkBagExportConfig(TestCase):
 
         entity = {
             'heeftEenRelatieMetVerblijfsobject.[0].identificatie': None,
-            'heeftEenRelatieMetVerblijfsobject.[0].status.code': 20,
             'heeftEenRelatieMetVerblijfsobject.[0].broninfo.woonplaatsnaam': 'Amsterdam'
         }
 
@@ -32,31 +30,9 @@ class TestBrkBagExportConfig(TestCase):
                                                     'and Amsterdam')
 
         entity = {
-            'heeftEenRelatieMetVerblijfsobject.[0].identificatie': 'some id',
-            'heeftEenRelatieMetVerblijfsobject.[0].status.code': 20,
-            'heeftEenRelatieMetVerblijfsobject.[0].broninfo.woonplaatsnaam': 'not Amsterdam'
-        }
-
-        self.assertFalse(vot_filter.filter(entity), 'Should return False when VOT identification set and status code '
-                                                    'is not included in valid_status_codes')
-
-        entity = {
             'heeftEenRelatieMetVerblijfsobject.[0].identificatie': None,
-            'heeftEenRelatieMetVerblijfsobject.[0].status.code': 20,
             'heeftEenRelatieMetVerblijfsobject.[0].broninfo.woonplaatsnaam': None,
         }
 
         self.assertTrue(vot_filter.filter(entity), 'Should return True when VOT identification not set and city '
                                                    'not set')
-
-        status_codes = [2, 3, 4, 6]
-
-        for status_code in status_codes:
-            entity = {
-                'heeftEenRelatieMetVerblijfsobject.[0].identificatie': 'some id',
-                'heeftEenRelatieMetVerblijfsobject.[0].status.code': status_code,
-                'heeftEenRelatieMetVerblijfsobject.[0].broninfo.woonplaatsnaam': 'not Amsterdam'
-            }
-
-            self.assertTrue(vot_filter.filter(entity), 'Should return True when VOT identification set and code '
-                                                       'incluced in valid_status_codes')
