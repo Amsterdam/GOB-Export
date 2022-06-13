113352ed454ea57de70d734e869b0931e0b58e13
Jaap Koetsier <jaap@chariot-it.nl>
2022-06-13 13:39:26 +0200

    47472: Show line numbers when logging duplicate values in CSV in export test

--- src/tests/test_csv_inspector.py
+++ src/tests/test_csv_inspector.py
@@ -76,50 +76,55 @@ class TestCSVInspector(TestCase):
         mock_logger.info.assert_called()
 
     @patch("gobexport.csv_inspector.logger")
-    def test_log_warning(self, mock_logger):
-        i = CSVInspector('any filename', '', {})
-
-        i.warnings = 0
-        i._log_warning("any key", "any value")
-        self.assertEqual(mock_logger.warning.call_count, 1)
-        self.assertEqual(i.warnings, 1)
-
-        mock_logger.warning.reset_mock()
-        i.warnings = i.MAX_WARNINGS - 2
-        i._log_warning("any key", "any value")
-        self.assertEqual(mock_logger.warning.call_count, 1)
-
-        mock_logger.warning.reset_mock()
-        i.warnings = i.MAX_WARNINGS - 1
-        i._log_warning("any key", "any value")
-        self.assertEqual(mock_logger.warning.call_count, 2)
-
-        mock_logger.warning.reset_mock()
-        i.warnings = i.MAX_WARNINGS
-        i._log_warning("any key", "any value")
-        mock_logger.warning.assert_not_called()
-
-    @patch("gobexport.csv_inspector.logger", MagicMock())
-    def test_check_uniqueness(self):
+    def test_collect_values_for_uniquess_check(self, mock_logger):
         i = CSVInspector('any filename', '', {
             'unique_cols': [[1], [2, 3]]
         })
-        i._log_warning = MagicMock()
 
-        i._check_uniqueness(['a', 'b', 'c'])
-        i._log_warning.assert_not_called()
+        i._collect_values_for_uniquess_check(['a', 'b', 'c'], 1)
+        self.assertEqual({
+            '[1]': {
+                'a': [1]
+            },
+            '[2, 3]': {
+                'b.c': [1]
+            }
+        }, i.unique_values)
 
-        i._log_warning.reset_mock()
-        i._check_uniqueness(['a', 'b', 'd'])
-        self.assertEqual(i._log_warning.call_count, 1)
+        i._collect_values_for_uniquess_check(['a', 'b', 'd'], 2)
+        self.assertEqual({
+            '[1]': {
+                'a': [1, 2]
+            },
+            '[2, 3]': {
+                'b.c': [1],
+                'b.d': [2]
+            }
+        }, i.unique_values)
 
-        i._log_warning.reset_mock()
-        i._check_uniqueness(['b', 'b', 'd'])
-        self.assertEqual(i._log_warning.call_count, 1)
+        i._collect_values_for_uniquess_check(['b', 'b', 'd'], 3)
+        self.assertEqual({
+            '[1]': {
+                'a': [1, 2],
+                'b': [3]
+            },
+            '[2, 3]': {
+                'b.c': [1],
+                'b.d': [2, 3]
+            }
+        }, i.unique_values)
 
-        i._log_warning.reset_mock()
-        i._check_uniqueness(['b', 'b', 'd'])
-        self.assertEqual(i._log_warning.call_count, 2)
+        i._collect_values_for_uniquess_check(['b', 'b', 'd'], 4)
+        self.assertEqual({
+            '[1]': {
+                'a': [1, 2],
+                'b': [3, 4]
+            },
+            '[2, 3]': {
+                'b.c': [1],
+                'b.d': [2, 3, 4]
+            }
+        }, i.unique_values)
 
     def test_check_lengths(self):
         i = CSVInspector('any filename', '', {})
@@ -141,22 +146,22 @@ class TestCSVInspector(TestCase):
 
     def test_check_columns(self):
         i = CSVInspector('any filename', '', {})
-        i._check_uniqueness = MagicMock()
+        i._collect_values_for_uniquess_check = MagicMock()
         i._check_lengths = MagicMock()
-        i.check_columns('any columns')
-        i._check_uniqueness.assert_called_with('any columns')
+        i._check_columns('any columns', 2)
+        i._collect_values_for_uniquess_check.assert_called_with('any columns', 2)
         i._check_lengths.assert_called_with('any columns')
 
     def test_check_lines(self):
         i = CSVInspector('any filename', '', {})
-        i.check_columns = MagicMock()
+        i._check_columns = MagicMock()
         lines = [
             'HEADERS',
             'A;B;C\n'
         ]
 
         i.check_lines(lines)
-        i.check_columns.assert_called_with(['A', 'B', 'C'])
+        i._check_columns.assert_called_with(['A', 'B', 'C'], 2)
 
     @patch("gobexport.csv_inspector.logger")
     def test_check_lines_unique(self, mock_logger):
@@ -173,8 +178,8 @@ class TestCSVInspector(TestCase):
         res = i.check_lines(lines)
 
         mock_logger.warning.assert_has_calls([
-            call("Non unique value found for ['a', 'b']: 1.2"),
-            call("Non unique value found for ['c']: 3"),
+            call("Non unique value found for ['a', 'b']: 1.2 on lines 2,4"),
+            call("Non unique value found for ['c']: 3 on lines 2,5"),
         ])
 
         self.assertEqual({
@@ -187,3 +192,71 @@ class TestCSVInspector(TestCase):
             'minlength_col_3': 1,
             'maxlength_col_3': 1
         }, res)
+
+    @patch("gobexport.csv_inspector.logger")
+    def test_check_lines_unique_max_warnings(self, mock_logger):
+        lines = [
+            'a;b;c',
+            '1;2;2',
+            '1;3;4',
+            '1;2;2',
+            '2;3;3',
+            '1;2;3',
+            '1;3;4',
+            '1;2;2',
+            '2;3;3',
+        ]
+
+        i = CSVInspector('any filename', lines[0], {'unique_cols': [['c']]})
+        i.MAX_WARNINGS = 2
+
+        res = i.check_lines(lines)
+
+        mock_logger.warning.assert_has_calls([
+            call("Found more than 2 duplicated values for ['c']. Logging first 2 values."),
+            call("Non unique value found for ['c']: 2 on lines 2,4,8"),
+            call("Non unique value found for ['c']: 4 on lines 3,7"),
+        ])
+
+        self.assertEqual({
+            "['c']_is_unique": False,
+            'minlength_col_1': 1,
+            'maxlength_col_1': 1,
+            'minlength_col_2': 1,
+            'maxlength_col_2': 1,
+            'minlength_col_3': 1,
+            'maxlength_col_3': 1
+        }, res)
+
+    def test_filter_non_uniques(self):
+        i = CSVInspector('any filename', '', {})
+
+        non_filtered = {
+            'key1': {
+                'value1': [1, 2],
+                'value2': [3],
+                'value3': [4, 7],
+                'value4': [5],
+            },
+            'key2': {
+                'value1': [],
+                'value2': [2],
+            },
+            'key3': {
+                'value1': [1, 2, 3, 5],
+            }
+        }
+
+        expected_result = {
+            'key1': {
+                'value1': [1, 2],
+                'value3': [4, 7],
+            },
+            'key2': {
+            },
+            'key3': {
+                'value1': [1, 2, 3, 5],
+            }
+        }
+
+        self.assertEqual(expected_result, i._filter_non_uniques(non_filtered))
