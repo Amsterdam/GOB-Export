d4fe48141ecd3387f2953da6c93445ed0814f25a
Peter Adrichem <Peter.Adrichem@gmail.com>
2023-07-07 16:22:53 +0200

    BRK2 aantekeningen CSV exports. (#834)

---
+++ src/gobexport/exporter/config/brk2/aantekeningen.py
@@ -0,0 +1,195 @@
+"""BRK2 aantekeningenrechten exports."""
+
+
+from gobexport.exporter.config.brk2.utils import brk2_filename
+from gobexport.exporter.csv import csv_exporter
+from gobexport.exporter.shared.brk import format_timestamp
+from gobexport.filters.notempty_filter import NotEmptyFilter
+
+
+class AantekeningenExportConfig:
+    """Aantekeningenrechten/Aantekeningenkadastraleobjecten export configuration."""
+
+    art_query = """
+{
+  brk2Aantekeningenrechten {
+    edges {
+      node {
+        identificatie
+        aard
+        omschrijving
+        einddatum
+        heeftBrkBetrokkenPersoon {
+          edges {
+            node {
+              identificatie
+            }
+          }
+        }
+        betrokkenBrkTenaamstelling {
+          edges {
+            node {
+              identificatie
+              vanBrkZakelijkRecht {
+                edges {
+                  node {
+                    aardZakelijkRecht
+                    rustOpBrkKadastraalObject {
+                      edges {
+                        node {
+                          identificatie
+                          perceelnummer
+                          indexletter
+                          indexnummer
+                          aangeduidDoorBrkKadastralegemeentecode {
+                            edges {
+                              node {
+                                identificatie
+                              }
+                            }
+                          }
+                          aangeduidDoorBrkKadastralesectie {
+                            edges {
+                              node {
+                                code
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+    art_format = {
+        "BRK_ATG_ID": "identificatie",
+        "ATG_AARDAANTEKENING_CODE": "aard.code",
+        "ATG_AARDAANTEKENING_OMS": "aard.omschrijving",
+        "ATG_OMSCHRIJVING": "omschrijving",
+        "ATG_EINDDATUM": {
+            "action": "format",
+            "formatter": format_timestamp,
+            "value": "einddatum",
+        },
+        "ATG_TYPE": {"action": "literal", "value": "Aantekening Zakelijk Recht (R)"},
+        "BRK_KOT_ID": "rustOpBrkKadastraalObject.[0].identificatie",
+        "KOT_KADASTRALEGEMCODE_CODE": "aangeduidDoorBrkKadastralegemeentecode.identificatie",
+        "KOT_SECTIE": "aangeduidDoorBrkKadastralesectie.code",
+        "KOT_PERCEELNUMMER": "rustOpBrkKadastraalObject.[0].perceelnummer",
+        "KOT_INDEX_LETTER": "rustOpBrkKadastraalObject.[0].indexletter",
+        "KOT_INDEX_NUMMER": "rustOpBrkKadastraalObject.[0].indexnummer",
+        "BRK_TNG_ID": "betrokkenBrkTenaamstelling.[0].identificatie",
+        "ZRT_AARD_ZAKELIJKRECHT_CODE": "vanBrkZakelijkRecht.[0].aardZakelijkRecht.code",
+        "ZRT_AARD_ZAKELIJKRECHT_OMS": "vanBrkZakelijkRecht.[0].aardZakelijkRecht.omschrijving",
+        "BRK_SJT_ID": "heeftBrkBetrokkenPersoon.[0].identificatie",
+    }
+
+    akt_query = """
+{
+  brk2Aantekeningenkadastraleobjecten {
+    edges {
+      node {
+        identificatie
+        aard
+        omschrijving
+        einddatum,
+        heeftBrkBetrokkenPersoon {
+          edges {
+            node {
+              identificatie
+            }
+          }
+        }
+        heeftBetrekkingOpBrkKadastraalObject {
+          edges {
+            node {
+              identificatie
+              perceelnummer
+              indexletter
+              indexnummer
+              aangeduidDoorBrkKadastralegemeentecode {
+                edges {
+                  node {
+                    identificatie
+                  }
+                }
+              }
+              aangeduidDoorBrkKadastralesectie {
+                edges {
+                  node {
+                    code
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+    akt_format = {
+        "BRK_ATG_ID": "identificatie",
+        "ATG_AARDAANTEKENING_CODE": "aard.code",
+        "ATG_AARDAANTEKENING_OMS": "aard.omschrijving",
+        "ATG_OMSCHRIJVING": "omschrijving",
+        "ATG_EINDDATUM": {
+            "action": "format",
+            "formatter": format_timestamp,
+            "value": "einddatum",
+        },
+        "ATG_TYPE": {"action": "literal", "value": "Aantekening Kadastraal object (O)"},
+        "BRK_KOT_ID": "heeftBetrekkingOpBrkKadastraalObject.[0].identificatie",
+        "KOT_KADASTRALEGEMCODE_CODE": "aangeduidDoorBrkKadastralegemeentecode.identificatie",
+        "KOT_SECTIE": "aangeduidDoorBrkKadastralesectie.code",
+        "KOT_PERCEELNUMMER": "heeftBetrekkingOpBrkKadastraalObject.[0].perceelnummer",
+        "KOT_INDEX_LETTER": "heeftBetrekkingOpBrkKadastraalObject.[0].indexletter",
+        "KOT_INDEX_NUMMER": "heeftBetrekkingOpBrkKadastraalObject.[0].indexnummer",
+        "BRK_TNG_ID": "",
+        "ZRT_AARD_ZAKELIJKRECHT_CODE": "",
+        "ZRT_AARD_ZAKELIJKRECHT_OMS": "",
+        "BRK_SJT_ID": "heeftBrkBetrokkenPersoon.[0].identificatie",
+    }
+
+    products = {
+        "csv_art": {
+            "api_type": "graphql_streaming",
+            "secure_user": "gob",
+            "unfold": True,
+            "cross_relations": True,
+            "entity_filters": [
+                NotEmptyFilter("betrokkenBrkTenaamstelling.[0].identificatie"),
+                NotEmptyFilter("rustOpBrkKadastraalObject.[0].identificatie"),
+            ],
+            "exporter": csv_exporter,
+            "query": art_query,
+            "filename": lambda: brk2_filename("aantekening", use_sensitive_dir=True),
+            "mime_type": "plain/text",
+            "format": art_format,
+        },
+        "csv_akt": {
+            "api_type": "graphql_streaming",
+            "secure_user": "gob",
+            "unfold": True,
+            "entity_filters": [
+                NotEmptyFilter("heeftBetrekkingOpBrkKadastraalObject.[0].identificatie"),
+            ],
+            "exporter": csv_exporter,
+            "query": akt_query,
+            "filename": lambda: brk2_filename("aantekening", use_sensitive_dir=True),
+            "mime_type": "plain/text",
+            "format": akt_format,
+            "append": True,
+        },
+    }
