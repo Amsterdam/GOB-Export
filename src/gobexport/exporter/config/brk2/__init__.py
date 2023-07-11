7480c8a8a3755c34f0d2d54f6a4fc4309cd505fa
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-07-11 14:26:41 +0200

    Add brk2 stukdelen export config

--- src/gobexport/exporter/config/brk2/__init__.py
+++ src/gobexport/exporter/config/brk2/__init__.py
@@ -5,3 +5,4 @@ from gobexport.exporter.config.brk2.gemeentes import GemeentesExportConfig
 from gobexport.exporter.config.brk2.kadastralegemeentecodes import KadastralegemeentecodesExportConfig
 from gobexport.exporter.config.brk2.kadastralesecties import KadastralesectiesExportConfig
 from gobexport.exporter.config.brk2.zakelijkerechten import ZakelijkerechtenExportConfig
+from gobexport.exporter.config.brk2.stukdelen import StukdelenExportConfig
