f42d5387f551f742fb4327ce1b47bddd7082f14e
JKoetsier <jaap@chariot-it.nl>
2023-12-08 09:00:19 +0100

    Add setup.py to repo (#874)
    * Add setup.py
    
    * Update install requires
    
    * Remove unnecessary pathlib import from setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GOB-Export",
+    version="0.1",
+    url="https://github.com/Amsterdam/GOB-Export.git",
+    license="MPL2",
+    author="Amsterdam",
+    author_email="datapunt@amsterdam.nl",
+    description="GOB-Export",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "Flask==2.3.2",
+        "Flask-Cors==3.0.10",
+        "pysftp==0.2.9",
+        "freezegun==1.2.2",
+        "requests-mock~=1.11.0",
+        "gobconfig @ git+https://github.com/Amsterdam/GOB-Config.git@v0.14.2",
+        "gobcore @ git+https://github.com/Amsterdam/GOB-Core.git@v2.23.0"
+    ]
+)
