94184ef71852c85c1b70f2e38ac1e93991e542ed
Jaap Koetsier <jaap@chariot-it.nl>
2024-02-12 09:45:11 +0100

    Fix format_bedrag in export SDL

--- src/tests/exporter/config/brk2/test_stukdelen.py
+++ src/tests/exporter/config/brk2/test_stukdelen.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
 
-from gobexport.exporter.config.brk2.stukdelen import StukdelenExportConfig
+from gobexport.exporter.config.brk2.stukdelen import format_bedrag
 
 
 class TestStukdelenExportConfig(TestCase):
 
     def test_format_bedrag(self):
-        exporter = StukdelenExportConfig()
-        self.assertEqual('100', exporter.format_bedrag('100.0'))
-        self.assertEqual('125', exporter.format_bedrag('124.6'))
-        self.assertEqual('124', exporter.format_bedrag('124.4'))
+        self.assertEqual('100', format_bedrag('100.0'))
+        self.assertEqual('125', format_bedrag('124.6'))
+        self.assertEqual('124', format_bedrag('124.4'))
